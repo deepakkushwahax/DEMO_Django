e3ab5d4fd673f03ee93b9bc7537e753ceb8b82c5
Deepak Kushwaha <88443379+deepakkushwahax@users.noreply.github.com>
2024-03-23 20:36:00 +0530

    Added project

--- firstProject/testapp/views.py
+++ firstProject/testapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 
-# Create your views here.
+def greetings(request):
+    s = "Hello and Welcome to first view"
+    return HttpRequest(s)
