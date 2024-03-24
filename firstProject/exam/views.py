343138669da5acd42b56de5274928320696e2762
Deepak Kushwaha <88443379+deepakkushwahax@users.noreply.github.com>
2024-03-24 07:58:27 +0530

    added urls in app level

--- firstProject/exam/views.py
+++ firstProject/exam/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 def testpaper(request):
-    s = "<h1>This is a test paper</h1>"
-    return HttpResponse(s)
+    template = loader.get_template('testpaper.html')
+    res = template.render()
+    return HttpResponse(res)
 
 def result(request):
     s = "<h1>This is your test result</h1>"
