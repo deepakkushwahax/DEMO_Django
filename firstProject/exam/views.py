49dfe1b04a11101e5b239b6ed2fe611406fe31c3
Deepak Kushwaha <88443379+deepakkushwahax@users.noreply.github.com>
2024-03-25 20:37:25 +0530

    ADDED NINJA2 IN HTML

--- firstProject/exam/views.py
+++ firstProject/exam/views.py
@@ -5,8 +5,20 @@ from django.template import loader
 # Create your views here.
 
 def testpaper(request):
+    que="who Developed Python?"
+    a="Dennis Ritchie"
+    b="Guido Van Rossum"
+    c="Ken Thompson"
+    d="Bjarne Stroutrup"
+    context={
+        'que':que,
+        'a':a,
+        'b':b,
+        'c':c,
+        "d":d,
+    }
     template = loader.get_template('testpaper.html')
-    res = template.render()
+    res = template.render(context,request)
     return HttpResponse(res)
 
 def result(request):
