5a04370a343ec57af0e543145e20c519170a6fac
Deepak Kushwaha <88443379+deepakkushwahax@users.noreply.github.com>
2024-03-26 15:37:20 +0530

    added admin by migrates from the server

--- firstProject/testapp/models.py
+++ firstProject/testapp/models.py
@@ -6,4 +6,7 @@ class Employee(models.Model):
     eno = models.IntegerField()
     ename = models.CharField(max_length=50)
     esal = models.FloatField()
+    def __str__(self):
+        return self.ename
+        
 
